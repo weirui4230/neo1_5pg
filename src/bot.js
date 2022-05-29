b643dfe1b4a00cb0252e18f07b25cb28a2debeeb
Wei Rui <123106weirui26@gmail.com>
2022-05-29 14:25:24 +0800

    Added a private data for our bot token

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,10 @@
+import { config } from 'dotenv';
+config({ path: '.env' });
+
 import { Client, Intents } from 'discord.js';
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 bot.on('ready', ()=> console.log('The bot is here :D'));
 
-bot.login('OTM0Njc4MTIzMTgwNTk3Mjc4.Gkanvf.E8Sy7ElJdT7A8KbA-9OlUQs83U5YMAxGFhlD3k');
+bot.login(process.env.BOT_TOKEN);
