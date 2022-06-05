dded66afd3b6f477a6a4692c2590bd80c7b98adc
Wei Rui <123106weirui26@gmail.com>
2022-06-05 14:11:34 +0800

    added the reply feature to our bot

--- src/bot.js
+++ src/bot.js
@@ -6,5 +6,24 @@ import { Client, Intents } from 'discord.js';
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 bot.on('ready', ()=> console.log('The bot is here :D'));
+bot.on('message', async (msg) => {
+
+    //if(msg.author.bot)return;
+
+    //await msg .reply("Hello :D");
+    // this is so that my bot will reply specifically
+    if(msg.content.startsWith("wr")){
+        msg .reply("Hello . I am WR Bot ヾ(≧▽≦*)o");
+    }
+
+    //this is the bot reply me in direct message
+    if(msg.content.startsWith("DM")){
+        msg.member.send("Hi ☆*: .｡. o(≧▽≦)o .｡.:*☆");
+    }
+});
 
 bot.login(process.env.BOT_TOKEN);
+
+
+
+
